7eba3f2a33fde7d43fec63170d4a2920cb3f551e
Diego <diegofernandezl2001@gmail.com>
2024-12-02 13:11:11 +0100

    Arreglo vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src", // Cambia la raíz del proyecto a "src"
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        login: resolve(__dirname, "src/login.html"),
+      },
+    },
     outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
   },
+  base: "./",
 });
